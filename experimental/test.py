0d1cafb526a2506c065f8862f689d53e06bda8d7
TedSjoblom <36690474+TedSjoblom@users.noreply.github.com>
2024-08-30 21:22:16 +0200

    0.3.7-pre.58

---
+++ experimental/test.py
@@ -0,0 +1,10 @@
+from keelson.payloads.Target_pb2 import Target
+
+if __name__ == '__main__':
+    target = Target()
+    
+    target.mmsi = 123456789
+    target.navigation_status = Target.NavigationStatus.MOORED
+    print(target.ListFields())
+
+    print(target)
